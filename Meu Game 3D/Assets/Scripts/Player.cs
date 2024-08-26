cb7d238e522f19cb898003f588a99cbbcc449866
nivanyy <silva.nivany@escolar.ifrn.edu.br>
2024-08-26 11:51:31 -0300

    26/08 quarto video feito.

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,27 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
+
 
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forcaPulo = 1;
+    private bool noChao;
+
+
     public Rigidbody rb;
-    // Start is called before the first frame update
+
     void Start()
     {
         Debug.Log("start");
         TryGetComponent(out rb);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (! noChao && collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }
+    }
+
+void Update()
     {
         Debug.Log("update");
         float x = Input.GetAxis("Horizontal");
-         float y = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Vertical");
 
-         Vector3 direcao = new Vector3(x,0,y);
-         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);   
-     }
+        Vector3 direcao = new Vector3(x, 0, y);
+        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+
+
+
+
+    if (transform.position.y <= -10)
+        {
+            //jogador caiu 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+}
 }
